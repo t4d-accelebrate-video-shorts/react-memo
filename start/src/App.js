84e51570fc6e4381e6f21d7b4fbe938c18679071
Eric Greene <eric@t4d.io>
2019-09-01 22:39:55 -0400

    add start code

--- start/src/App.js
+++ start/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { carsPropType } from './propTypes';
+import { ToolHeader, CarTable, CarForm } from './components';
 
-export default App;
+export const App = ({
+  cars: initialCars,
+}) => {
+
+  const [ cars, setCars ] = useState(initialCars.concat());
+  const [ editCarId, setEditCarId ] = useState(-1);
+
+  const appendCar = car => {
+    setCars(cars.concat({
+      ...car,
+      id: Math.max(...cars.map(c => c.id), 0) + 1,
+    }));
+    setEditCarId(-1);
+  };
+
+  const replaceCar = car => {
+    const carIndex = cars.findIndex(c => c.id === car.id);
+    const newCars = cars.concat();
+    newCars[carIndex] = car;
+    setCars(newCars);
+    setEditCarId(-1);
+  };
+
+  const deleteCar = carId => {
+    setCars(cars.filter(c => c.id !== carId));
+    setEditCarId(-1);
+  };
+
+  const cancelCar = () => {
+    setEditCarId(-1);
+  };
+
+  return <>
+    <ToolHeader headerText="Car Tool" />
+    <CarTable cars={cars} editCarId={editCarId}
+      onEditCar={setEditCarId} onDeleteCar={deleteCar}
+      onSaveCar={replaceCar} onCancelCar={cancelCar} />
+    <CarForm buttonText="Add Car" onSubmitCar={appendCar} />
+  </>;
+};
+
+App.defaultProps = {
+  cars: [],
+};
+
+App.propTypes = {
+  cars: carsPropType,
+};
